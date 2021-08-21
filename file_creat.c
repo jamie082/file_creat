cc2b4b23f22c9c6e2e143e6a5b7105118ab462aa
jamie082 <jamiemorrissey02@gmail.com>
2021-08-21 15:31:00 +0000

    updated source

--- file_creat.c
+++ file_creat.c
@@ -7,7 +7,7 @@ extern int errno;
 
 int main (void)
 {
-	const char *path = "temp.file";
+	char *path = "temp.file";
 
 	printf("Creating file...'%s'\n", path);
 
@@ -16,24 +16,41 @@ int main (void)
 		printf("True\n");
 	}
 
-	if (chmod(path, S_IRUSR)) // True
+	else if (chmod(path, S_IRUSR)) // True
 	{
 		printf("Directory is write protected\n"); // only printing if directory is -w
 		printf("%d ", errno);
 		printf("%s\n", strerror(errno));
 	}
 
-	else if (open(path, O_CREAT) != 0)  // (open(path, O_CREAT) == 1)
-	{
-		printf("File created\n");
-	}
+	/* output mode of file */
 
-	/* simple error handeling, if directory is write permission  */
-
-	else
+	else if (open(path, O_CREAT) == -1)
 	{
-		printf("%d ", errno);
-		printf("%s\n", strerror(errno));
+		if (fd = 1)
+		{
+			printf("File created\n");
+		}
+
+		switch (S_IRUSR) // print out permissions of temp.file
+		{
+			case S_IRWXU:
+				printf("read, write, and execute by user (owner)");
+				break;
+			case S_IRWXG:
+				printf("read, write, and execute by group");
+				break;
+			case S_IRWXO:
+				printf("read, write, and execute by other (world)");
+				break;
+		}
+
+		else
+		{
+			printf("Directory is write protected\n");
+			printf("%d ", errno);
+			printf("%s\n", strerror(errno));
+		}
 	}
 
 	exit(1);
